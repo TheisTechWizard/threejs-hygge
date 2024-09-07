2a0b6c641bee4fc77693587cbef454abe393b7d5
Theis Nielsen Haahr <Theishaahr@yahoo.dk>
2024-09-07 10:57:19 +0200

    threejs setup

---
+++ src/views/ModelView.vue
@@ -0,0 +1,19 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<script setup>
+import { Scene } from 'three'
+
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+</script>
+<style lang="">
+
+</style>
